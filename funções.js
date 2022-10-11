a85ed69028456f1300b788086d0ac26f4717d376
Cristian Gluchak Colaços <106543901+Crist1anJ@users.noreply.github.com>
2022-10-11 10:50:24 -0300

    Update funções.js

--- funções.js
+++ funções.js
@@ -25,12 +25,41 @@ function Idade()
     return idade;
 };
 
+
+
+
+function calcIdade(data) {
+    var d = new Date,
+        ano_atual = d.getFullYear(),
+        mes_atual = d.getMonth() + 1,
+        dia_atual = d.getDate(),
+        split = data.split('/'),
+        novadata = split[1] + "/" +split[0]+"/"+split[2],
+        data_americana = new Date(novadata),
+        vAno = data_americana.getFullYear(),
+        vMes = data_americana.getMonth() + 1,
+        vDia = data_americana.getDate(),
+        ano_aniversario = +vAno,
+        mes_aniversario = +vMes,
+        dia_aniversario = +vDia,
+        quantos_anos = ano_atual - ano_aniversario;
+    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--;
+    }
+    return quantos_anos < 0 ? 0 : quantos_anos;
+};
+
+
+
+
+
 function alerta()
 {
     nome = Nome();
     telefone = Telefone();
     email= Email();
     idade=Idade();
+    vidade=calcIdade(data);
     mensagem = Mensagem();
     alert("Olá " + nome.value + ", Seus dados foram salvos!"+
     "\nNome: " +nome.value+
@@ -38,5 +67,6 @@ function alerta()
     +"\nEmail: "+email.value
     +"\nIdade: "+idade.value
     +"\nMensagem do Cliente: "+mensagem.value
+    +"\nIdade calculada: "+vidade.value
     );
 };
